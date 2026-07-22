8540bb345907a88a85b5229737a343ec75a328b0
Aditya Paswan <143643204+Aditya0482@users.noreply.github.com>
2026-07-22 22:23:34 +0530

    Update prime_no.py

--- prime_no.py
+++ prime_no.py
@@ -1,1 +1,9 @@
-
+count=0
+num=int(input("Enter the number : "))
+for i in range(1,num+1):
+  if num%i==0:
+    count+=1
+if count==2:
+  print(f" {num} is prime number")
+else:
+  print(f"{num} is not a prime number")
