d0db74602ad8f5b0d6d1f827d53c2331add9cddd
Aditya Paswan <143643204+Aditya0482@users.noreply.github.com>
2026-07-23 14:48:43 +0530

    Update Largest_numbers.py

--- Largest_among_n_numbers.py
+++ Largest_among_n_numbers.py
@@ -1,1 +1,9 @@
+li=[]
+num=int(input("Enter the n numbers : "))
 
+for n in range(num):
+  numbers=int(input("Enter the numbers : "))
+  li.append(numbers)
+
+print("Maximum number is : ",max(li))
+print("Minimum number is : ",min(li))
