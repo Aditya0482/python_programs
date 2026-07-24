955d741f082bc9afbe443a78f64e94df2724c6d4
Aditya Paswan <143643204+Aditya0482@users.noreply.github.com>
2026-07-24 23:23:17 +0530

    Create Swapping2.py

---
+++ Swapping2.py
@@ -0,0 +1,9 @@
+a=int(input("Ebther the value of a : "))
+b=int(input("Enter the value of b : "))
+
+print("Befire swapping :\n a = ",a,"b = ",b)
+a=a+b
+b=a-b
+a=a-b
+
+print("After swapping :\na = ",a,"b = ",b)
